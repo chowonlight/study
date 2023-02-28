e3e6741dc1a2d3fc05373c038d5c05cb406ddf5d
Jangsu Jeon <126481085+chowonlight@users.noreply.github.com>
2023-02-28 20:24:44 +0900

    Add files via upload

---
+++ keras01_2.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+print("덴써플로 버전 : ", tf.__version__)
+
+
+
---
+++ keras04.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([1, 2, 3, 5, 4])
+
+model = Sequential()
+model.add(Dense(5, input_dim=1))   
+model.add(Dense(8))  
+model.add(Dense(10))     
+model.add(Dense(8)) 
+model.add(Dense(5)) 
+model.add(Dense(1)) 
+
+model.compile(loss='mae', optimizer='adam')
+model.fit(x, y, epochs=1000)
+
+loss = model.evaluate(x, y)
+print('loss = ', loss)
+
+result = model.predict([6])
+print("[6]의 예측값 = ", result)
+
