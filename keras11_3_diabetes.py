9d7c86db12e8f5d6260a4f62c661d036986e0fdc
Jangsu Jeon <126481085+chowonlight@users.noreply.github.com>
2023-03-03 20:50:34 +0900

    Add files via upload

---
+++ keras11_3_diabetes.py
@@ -0,0 +1,67 @@
+
+
+import numpy as np
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+
+
+#1. 데이터
+
+datasets = load_diabetes()
+x = datasets.data
+y = datasets.target
+
+
+# print(x.shape, y.shape)   # (442, 10) (442,)
+
+
+############## [실습] ########
+# R2 0.62 이상
+#############################
+
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, 
+    train_size=0.9,  
+    shuffle=True,
+    random_state=30000)
+
+
+#2. 모델 구성
+
+model = Sequential()
+model.add(Dense(12, input_dim=10))
+model.add(Dense(24))
+model.add(Dense(36))
+model.add(Dense(64))
+model.add(Dense(100))
+model.add(Dense(80))
+model.add(Dense(40))
+model.add(Dense(20))
+model.add(Dense(10))
+model.add(Dense(1))
+
+
+#3. 컴파일 훈련
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(x_train, y_train, epochs=180, batch_size=8)
+
+
+#4. 평가, 예측
+
+loss= model.evaluate(x_test, y_test)
+print('loss : ', loss) 
+
+y_predict = model.predict(x_test)
+
+from sklearn.metrics import r2_score
+r2 = r2_score(y_test, y_predict)
+print('r2 =', r2)
+
+
+#   r2 = 0.6825268715062958
+
