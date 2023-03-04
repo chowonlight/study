7e717ea4707fa3c7e68536ff94aabab54d25814b
Jangsu Jeon <126481085+chowonlight@users.noreply.github.com>
2023-03-04 17:04:04 +0900

    Add files via upload

--- keras11_1_boston.py
+++ keras11_1_boston.py
@@ -1,4 +1,13 @@
 
+############# < 정리된 실행 부분 > #################
+
+
+######### [실습] ############
+#     1. train 0.7
+#     2. R2 0.8 이상
+#############################
+
+
 import numpy as np
 
 from tensorflow.keras.models import Sequential
@@ -7,12 +16,65 @@ from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 
-
-#1. 데이터
-
 datasets = load_boston()
 x = datasets.data
 y = datasets.target
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, 
+    train_size=0.7,  
+    shuffle=True,
+    random_state=800)
+
+model = Sequential()
+model.add(Dense(16, input_dim=13))
+model.add(Dense(24))
+model.add(Dense(36))
+model.add(Dense(48))
+model.add(Dense(64))
+model.add(Dense(32))
+model.add(Dense(16))
+model.add(Dense(8))
+model.add(Dense(1))
+
+model.compile(loss='mae', optimizer='adam')
+model.fit(x_train, y_train, epochs=650, batch_size=10)
+
+loss= model.evaluate(x_test, y_test)
+print('loss : ', loss) 
+
+y_predict = model.predict(x_test)
+
+r2 = r2_score(y_test, y_predict)
+print('r2 =', r2)
+
+
+################ < 작업 결과 > #####################
+
+
+#  Epoch 650/650
+#  36/36 [==============================] - 0s 1ms/step - loss: 3.4802
+#  5/5 [==============================] - 0s 0s/step - loss: 3.3594
+#  loss :  3.3593509197235107
+#  5/5 [==============================] - 0s 961us/step
+#  r2 = 0.7347395847390887
+
+
+################ < 수업 내용 > #####################
+
+#  import numpy as np
+
+#  from tensorflow.keras.models import Sequential
+#  from tensorflow.keras.layers import Dense
+#  from sklearn.datasets import load_boston
+#  from sklearn.model_selection import train_test_split
+#  from sklearn.metrics import r2_score
+
+
+#1. 데이터
+
+#  datasets = load_boston()
+#  x = datasets.data
+#  y = datasets.target
 
 # print(x)
 # print(y)
@@ -33,43 +95,40 @@ y = datasets.target
 #############################
 
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, 
-    train_size=0.7,  
-    shuffle=True,
-    random_state=800)
+#  x_train, x_test, y_train, y_test = train_test_split(x, y, 
+#      train_size=0.7,  
+#      shuffle=True,
+#      random_state=800)
 
 
 #2. 모델 구성
 
-model = Sequential()
-model.add(Dense(16, input_dim=13))
-model.add(Dense(24))
-model.add(Dense(36))
-model.add(Dense(48))
-model.add(Dense(64))
-model.add(Dense(32))
-model.add(Dense(16))
-model.add(Dense(8))
-model.add(Dense(1))
+#  model = Sequential()
+#  model.add(Dense(16, input_dim=13))
+#  model.add(Dense(24))
+#  model.add(Dense(36))
+#  model.add(Dense(48))
+#  model.add(Dense(64))
+#  model.add(Dense(32))
+#  model.add(Dense(16))
+#  model.add(Dense(8))
+#  model.add(Dense(1))
 
 
 #3. 컴파일 훈련
 
-model.compile(loss='mae', optimizer='adam')
-model.fit(x_train, y_train, epochs=650, batch_size=10)
+#  model.compile(loss='mae', optimizer='adam')
+#  model.fit(x_train, y_train, epochs=650, batch_size=10)
 
 
 #4. 평가, 예측
 
-loss= model.evaluate(x_test, y_test)
-print('loss : ', loss) 
+#  loss= model.evaluate(x_test, y_test)
+#  print('loss : ', loss) 
 
-y_predict = model.predict(x_test)
+#  y_predict = model.predict(x_test)
 
-r2 = r2_score(y_test, y_predict)
-print('r2 =', r2)
-
-
-#   r2 = 0.7337310169785668
+#  r2 = r2_score(y_test, y_predict)
+#  print('r2 =', r2)
 #
-
+#
