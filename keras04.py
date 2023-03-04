fe282ae88168ad71c3d97deb59c34f5d0fdea78c
Jangsu Jeon <126481085+chowonlight@users.noreply.github.com>
2023-03-04 11:24:50 +0900

    Add files via upload

--- keras04.py
+++ keras04.py
@@ -1,4 +1,6 @@
 
+############# < 정리한 실행 부분 > #################
+
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -15,7 +17,7 @@ model.add(Dense(5))
 model.add(Dense(1)) 
 
 model.compile(loss='mae', optimizer='adam')
-model.fit(x, y, epochs=1000)
+model.fit(x, y, epochs=7000)
 
 loss = model.evaluate(x, y)
 print('loss = ', loss)
@@ -23,3 +25,39 @@ print('loss = ', loss)
 result = model.predict([6])
 print("[6]의 예측값 = ", result)
 
+
+################ < 작업 결과 > #####################
+
+#  Epoch 7000/7000
+#  1/1 [==============================] - 0s 4ms/step - loss: 0.4014
+#  1/1 [==============================] - 0s 100ms/step - loss: 0.4015
+#  loss =  0.4015394151210785
+#  1/1 [==============================] - 0s 74ms/step
+#  [6]의 예측값 =  [[5.993847]]
+
+################ < 수업 내용 > #####################
+
+#  import numpy as np
+#  from tensorflow.keras.models import Sequential
+#  from tensorflow.keras.layers import Dense
+
+#  x = np.array([1, 2, 3, 4, 5])
+#  y = np.array([1, 2, 3, 5, 4])
+
+#  model = Sequential()
+#  model.add(Dense(5, input_dim=1))   
+#  model.add(Dense(8))  
+#  model.add(Dense(10))     
+#  model.add(Dense(8)) 
+#  model.add(Dense(5)) 
+#  model.add(Dense(1)) 
+
+#  model.compile(loss='mae', optimizer='adam')
+#  model.fit(x, y, epochs=7000)
+
+#  loss = model.evaluate(x, y)
+# print('loss = ', loss)
+
+# result = model.predict([6])
+# print("[6]의 예측값 = ", result)
+#
