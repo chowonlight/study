6fedbb4f55a1502317e2956efb76923f0196abb6
Jangsu Jeon <126481085+chowonlight@users.noreply.github.com>
2023-03-04 10:51:32 +0900

    Add files via upload

--- keras03_deep1.py
+++ keras03_deep1.py
@@ -1,31 +1,56 @@
-#1. 데이터
+
+############# < 정리한 실행 부분 > #################
+
 
 import numpy as np
-
-x = np.array([1,2,3])
-y = np.array([1,2,3])
-
-#2. 모델구성
-
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
+x = np.array([1,2,3])
+y = np.array([1,2,3])
+
 model = Sequential()
-model.add(Dense(3, input_dim=1))   #input layer   ctre + / -> #표시
+model.add(Dense(3, input_dim=1))  
 model.add(Dense(4))
 model.add(Dense(5))    
 model.add(Dense(3))     
-model.add(Dense(1))    #output layer         
+model.add(Dense(1))   
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(x, y, epochs=1000)
+
+
+################ < 수업 내용 > #####################
+
+
+#1. 데이터
+
+#  import numpy as np
+
+#  x = np.array([1,2,3])
+#  y = np.array([1,2,3])
+
+#2. 모델구성
+
+#  import tensorflow as tf
+#  from tensorflow.keras.models import Sequential
+#  from tensorflow.keras.layers import Dense
+
+#  model = Sequential()
+#  model.add(Dense(3, input_dim=1))   #input layer   ctre + / -> #표시
+#  model.add(Dense(4))
+#  model.add(Dense(5))    
+#  model.add(Dense(3))     
+#  model.add(Dense(1))    #output layer         
 
 #3. 컴파일, 훈련
 
-model.compile(loss='mse', optimizer='adam')
-model.fit(x, y, epochs=100)
+#  model.compile(loss='mse', optimizer='adam')
+#  model.fit(x, y, epochs=1000)
 
-# loss: 0.0111 
+# 결과 --> 1,000 번째 최종 loss
 
-
-
-
-
+# Epoch 1000/1000
+# 1/1 [==============================] - 0s 972us/step - loss: 0.0018
+#
