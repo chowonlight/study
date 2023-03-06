248edf8cce084e447406c72805212c4c60e150f2
Jangsu Jeon <126481085+chowonlight@users.noreply.github.com>
2023-03-07 08:13:04 +0900

    Update keras07_mlp6.py

--- keras07_mlp6.py
+++ keras07_mlp6.py
@@ -8,19 +8,19 @@ from tensorflow.keras.layers import Dense
 
 
 x=np.array([range(10), range(21, 31), range(201, 211)]) 
-w = x.T
+print(x.shape)  
+
+x = x.T
+print(x.shape)  
 
 y=np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
             [1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9],
             [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]]) 
-z = y.T 
-
-print(x.shape)  
-print(w.shape)  
 print(y.shape)  
-print(z.shape)  
-print()   
 
+
+y = y.T 
+print(y.shape)  
 
 model = Sequential()
 model.add(Dense(3, input_dim=3))
@@ -30,9 +30,9 @@ model.add(Dense(3))
 model.add(Dense(3))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(w, z, epochs=7000, batch_size=3)
+model.fit(x, y, epochs=7000, batch_size=3)
 
-loss=model.evaluate(w, z)
+loss=model.evaluate(x, y)
 print('loss= ', loss)
 
 result=model.predict([[9, 30, 210]])
