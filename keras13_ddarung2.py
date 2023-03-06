865747e4b17cdda4f66c2ce73dbdc2e041dcb05a
Jangsu Jeon <126481085+chowonlight@users.noreply.github.com>
2023-03-06 20:22:14 +0900

    Add files via upload

--- keras13_ddarung2.py
+++ keras13_ddarung2.py
@@ -22,21 +22,21 @@ y = train_csv['count']
 x_train, x_test, y_train, y_test = train_test_split(x, y, 
     train_size=0.7,  
     shuffle=True,
-    random_state=1500)
+    random_state=300)
 
 
 model = Sequential()
-model.add(Dense(24, input_dim=9))
-model.add(Dense(48))
-model.add(Dense(126))
-model.add(Dense(48))
-model.add(Dense(24))
+model.add(Dense(10, input_dim=9))
+model.add(Dense(20))
+model.add(Dense(30))
+model.add(Dense(40))
+model.add(Dense(30))
 model.add(Dense(12))
 model.add(Dense(1))
 
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, epochs=300, batch_size=10, verbose=0)
+model.fit(x_train, y_train, epochs=700, batch_size=80, verbose=0)
 
 
 loss= model.evaluate(x_test, y_test)
@@ -59,13 +59,13 @@ y_submit=model.predict(test_csv)
 
 submission = pd.read_csv(path+'submission.csv', index_col=0)
 submission['count'] = y_submit
-submission.to_csv(path + 'submit_0306_0505.csv')
+submission.to_csv(path + 'submit_0306_0506.csv')
 
 
 ################  < 작업 결과 >  ##################
 
 
-#### ( No.2 )  ##################
+#********* ( No.2 )  ***********
 
 # model = Sequential()
 # model.add(Dense(12, input_dim=9))
@@ -87,8 +87,7 @@ submission.to_csv(path + 'submit_0306_0505.csv')
 
 
 
-#### ( No.3 )  ##################
-
+#********* ( No.3 )  ***********
 
 # 모델은 동일함
 # submission.to_csv(path + 'submit_0306_0502.csv')
@@ -99,8 +98,7 @@ submission.to_csv(path + 'submit_0306_0505.csv')
 # RMSE :  47.995483663939254
 
 
-#### ( No.4 )  ##################
-
+#********* ( No.4 )  ***********
 
 # 모델은 동일함
 # submission.to_csv(path + 'submit_0306_0503.csv')
@@ -111,8 +109,7 @@ submission.to_csv(path + 'submit_0306_0505.csv')
 # RMSE :  47.978830525498765
 
 
-#### ( No.5 )  ##################
-
+#********* ( No.5 )  ***********
 
 # model = Sequential()
 # model.add(Dense(24, input_dim=9))
@@ -131,7 +128,29 @@ submission.to_csv(path + 'submit_0306_0505.csv')
 # RMSE :  47.93803721616891
 
 
-#### ( No.6 )  ##################
+#********* ( No.6 )  ***********
+
+# model = Sequential()
+# model.add(Dense(10, input_dim=9))
+# model.add(Dense(20))
+# model.add(Dense(30))
+# model.add(Dense(40))
+# model.add(Dense(30))
+# model.add(Dense(12))
+# model.add(Dense(1))
+
+# submission.to_csv(path + 'submit_0306_0505.csv')
+#     random_state=300)
+# model.fit(x_train, y_train, epochs=700, batch_size=80, verbose=0)
+
+# r2 = 0.6341166401424012
+# RMSE :  49.32214942862533
+
+
+#********* ( No.7 )  ***********
+
+
+
 
 
 
